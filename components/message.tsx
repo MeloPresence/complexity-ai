1f2f52166e84ef3f08f87d040b1a9b04f72b75fc
MeloPresence <80811174+MeloPresence@users.noreply.github.com>
2024-10-17 16:09:34 +0800

    refactor: extract chat message bubble components

---
+++ components/message.tsx
@@ -0,0 +1,103 @@
+import { AnimatedMarkdown as Markdown } from "@/components/markdown/markdown"
+import { Message } from "ai"
+import type { useChat } from "ai/react"
+
+const getTextFromDataUrl = (dataUrl: string): string => {
+  const base64 = dataUrl.split(",")[1]
+  // Yes, it's (Awful to Beautiful) for (Base64 to readable ASCII text)
+  return window.atob(base64)
+}
+
+const copyToClipboard = (text: string) => navigator.clipboard.writeText(text)
+
+export function ChatBubble({
+  message,
+  index,
+  className = "",
+  chat,
+}: {
+  message: Message
+  index: number
+  className?: string
+  chat: ReturnType<typeof useChat>
+}) {
+  const { isLoading, messages } = chat
+  return (
+    <div
+      className={`${className} group hover:bg-zinc-100 flex flex-col gap-1 px-4 w-full md:w-[500px] md:px-0 ${
+        index === 0 ? "mt-20" : ""
+      }`}
+    >
+      <div className="flex gap-2">
+        {/*Sender icon*/}
+        <div className="size-[24px] flex flex-col justify-center items-center flex-shrink-0 text-zinc-400">
+          {message.role === "assistant" ? "🤖" : "👤"}
+        </div>
+
+        {/*Message*/}
+        <div className="flex flex-col gap-1 max-w-full">
+          {/*Text content*/}
+          <div className="text-zinc-800 dark:text-zinc-300 flex flex-col gap-4 markdown">
+            {message.role === "assistant" ? (
+              <Markdown>{message.content}</Markdown>
+            ) : (
+              message.content
+            )}
+          </div>
+          {/*File attachments*/}
+          <div className="flex flex-row gap-2">
+            {message.experimental_attachments?.map((attachment) =>
+              attachment.contentType?.startsWith("image") ? (
+                <img
+                  className="rounded-md w-40 mb-3"
+                  key={attachment.name}
+                  src={attachment.url}
+                  alt={attachment.name}
+                />
+              ) : attachment.contentType?.startsWith("text") ? (
+                <div className="text-xs w-40 h-24 overflow-hidden text-zinc-400 border p-2 rounded-md dark:bg-zinc-800 dark:border-zinc-700 mb-3">
+                  {getTextFromDataUrl(attachment.url)}
+                </div>
+              ) : null,
+            )}
+          </div>
+        </div>
+      </div>
+
+      {/*Bottom bar context menu (hidden when last message is still streaming)*/}
+      {(index !== messages.length - 1 ||
+        (index === messages.length - 1 && !isLoading)) && (
+        <div className="flex gap-1 text-sm bg-zinc-200 invisible group-hover:visible">
+          <div className="text-xs">
+            Bottom bar context menu for{" "}
+            {message.role === "assistant" ? "🤖" : "👤"}
+          </div>
+
+          <button
+            className="bg-zinc-100"
+            onClick={() => copyToClipboard(message.content)}
+          >
+            Copy
+          </button>
+
+          {message.role === "assistant" ? (
+            <>{/*AI-specific actions*/}</>
+          ) : (
+            <>{/*User-specific actions*/}</>
+          )}
+        </div>
+      )}
+    </div>
+  )
+}
+
+export function LoadingChatBubble() {
+  return (
+    <div className="flex flex-row gap-2 px-4 w-full md:w-[500px] md:px-0">
+      <div className="size-[24px] flex flex-col justify-center items-center flex-shrink-0 text-zinc-400"></div>
+      <div className="flex flex-col gap-1 text-zinc-400">
+        <div>hmm...</div>
+      </div>
+    </div>
+  )
+}
