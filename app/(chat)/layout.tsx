21a988f85633f6dd7574d2ae3300f614202615ff
farahhhadi <farahhanimhadi@gmail.com>
2024-10-29 16:45:25 +0800

    UI:sidebar

--- app/(chat)/layout.tsx
+++ app/(chat)/layout.tsx
@@ -3,6 +3,8 @@
 import { useFirebaseUser } from "@/lib/firebase/user"
 import { type User } from "firebase/auth"
 import { useEffect, useState } from "react"
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
+import { AppSidebar } from "@/components/app-sidebar"
 
 export default function Layout({
   anonymous,
@@ -10,19 +12,32 @@ export default function Layout({
 }: {
   anonymous: React.ReactNode
   authenticated: React.ReactNode
-}) {
+})
+{
   const user: User | null | undefined = useFirebaseUser()
   const [isLoading, setIsLoading] = useState<boolean>(true)
   const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false)
+
   useEffect(() => {
     console.debug("(chat)/layout.tsx useEffect", { user })
     if (user === undefined) return
     setIsAuthenticated(Boolean(user && !user.isAnonymous && user.emailVerified))
     setIsLoading(false)
   }, [user])
+
   return isLoading ? (
     <div>Loading</div>
   ) : (
-    <>{isAuthenticated ? authenticated : anonymous}</>
-  )
+    <SidebarProvider>
+      <div className="flex h-screen overflow-hidden">
+      <AppSidebar />
+      <main className="flex-grow flex items-center justify-center p-6 w-full">
+        <div className="relative w-full max-w-3xl text-center">
+        <SidebarTrigger className="absolute top-4 left-1" />
+        {isAuthenticated ? authenticated : anonymous}
+        </div>
+      </main>
+      </div>
+    </SidebarProvider>
+  );
 }
