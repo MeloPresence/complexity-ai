d778d7b130d0f033fad04b07064eb88b8d775994
MeloPresence <80811174+MeloPresence@users.noreply.github.com>
2024-11-12 15:00:56 +0800

    refactor: move firebase credentials to environment variables

--- lib/server/firebase/app.ts
+++ lib/server/firebase/app.ts
@@ -1,4 +1,3 @@
-import serviceAccount from "@/secrets/serviceAccountKey.json"
 import {
   type App,
   cert,
@@ -10,6 +9,21 @@ import { getFirestore } from "firebase-admin/firestore"
 
 const FIREBASE_SERVER_APP_NAME = "serverApp"
 
+const serviceAccount = {
+  type: process.env.FIREBASE_CREDENTIALS_TYPE,
+  project_id: process.env.FIREBASE_CREDENTIALS_PROJECT_ID,
+  private_key_id: process.env.FIREBASE_CREDENTIALS_PRIVATE_KEY_ID,
+  private_key: process.env.FIREBASE_CREDENTIALS_PRIVATE_KEY,
+  client_email: process.env.FIREBASE_CREDENTIALS_CLIENT_EMAIL,
+  client_id: process.env.FIREBASE_CREDENTIALS_CLIENT_ID,
+  auth_uri: process.env.FIREBASE_CREDENTIALS_AUTH_URI,
+  token_uri: process.env.FIREBASE_CREDENTIALS_TOKEN_URI,
+  auth_provider_x509_cert_url:
+    process.env.FIREBASE_CREDENTIALS_AUTH_PROVIDER_X509_CERT_URL,
+  client_x509_cert_url: process.env.FIREBASE_CREDENTIALS_CLIENT_X509_CERT_URL,
+  universe_domain: process.env.FIREBASE_CREDENTIALS_UNIVERSE_DOMAIN,
+} as ServiceAccount
+
 let firebaseServerApp: App
 let isAppFirstInitialized = false
 
@@ -20,7 +34,7 @@ try {
   isAppFirstInitialized = true
   firebaseServerApp = initializeApp(
     {
-      credential: cert(serviceAccount as ServiceAccount),
+      credential: cert(serviceAccount),
     },
     FIREBASE_SERVER_APP_NAME,
   )
