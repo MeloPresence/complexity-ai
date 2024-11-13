59dd096951244fe5c07e2e6a23e0023f51889f11
MeloPresence <80811174+MeloPresence@users.noreply.github.com>
2024-11-13 10:31:58 +0800

    feat: add generate title api route

--- app/api/title/route.ts
+++ app/api/title/route.ts
@@ -1,3 +1,11 @@
-import type { NextRequest } from "next/server"
+import { generateTitle } from "@/actions/title"
+import type { ModdedCoreMessage } from "@/lib/server/message"
+import { type NextRequest, NextResponse } from "next/server"
 
-export async function GET(req: NextRequest) {}
+export async function GET(
+  req: NextRequest,
+): Promise<NextResponse<{ title: string }>> {
+  const { messages }: { messages: ModdedCoreMessage[] } = await req.json()
+  const title = await generateTitle(messages)
+  return NextResponse.json({ title })
+}
